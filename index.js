9505982ae41e11a4e2b6c0ebe651156b20f59a21
yaseminboyertan <yaseminboy@gmail.com>
2020-10-16 21:13:46 +0300

    Create main js file & implement two classes for meetup attending program
    Create main js file & implement two classes for meetup attending program

---
+++ index.js
@@ -0,0 +1,28 @@
+const Person =require('./person');
+const Meetup=require('./meetup');
+const Database=require('./database');
+
+const yasemin=new Person('Yasemin',37);
+const mert=new Person('Mert',8);
+
+yasemin.greet(mert);
+
+const wtmb=new Meetup('WTM Berlin');
+yasemin.attend(wtmb);
+wtmb.printAttendeesNames();
+
+//Database.save('meetup.json',wtmb);
+
+//const data=Database.load('meetup.json');
+Database.load('meetup.js',(err,data)=>{
+
+})
+console.log(data.name);
+
+const loadedMeetup= Meetup.create(data);            //new Meetup (data.name,data.attendees);
+
+const ulas=new Person('Ulaş',11);
+ulas.attend(loadedMeetup);
+wtmb.printAttendeesNames();
+console.log(loadedMeetup.attendees);
+//Database.save('person.json',yasemin);
