67e55cf9a56bf78e621dca77671503fe4333387e
yaseminboyertan <yaseminboy@gmail.com>
2020-10-16 21:15:59 +0300

    Convert classes to seperate js files as objects
    Convert classes to seperate js files as objects

---
+++ meetup.js
@@ -0,0 +1,17 @@
+module.exports=class Meetup
+{
+    constructor(name,attendees =[])
+    {
+        this.name=name;
+        this.attendees=attendees;
+    }
+    printAttendeesNames(){
+        this.attendees.forEach(printName);
+    }
+    static create({name,attendees}){
+        return new Meetup(name,attendees);
+    }
+}
+function printName (person){
+    console.log(person.name);
+}
