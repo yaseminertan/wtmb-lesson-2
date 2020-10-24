30e32045f4a985ee616d2e740990afae88af1550
yaseminboyertan <yaseminboy@gmail.com>
2020-10-24 18:37:17 +0300

    Implement axios usage examples
    Implement axios usage examples

--- lesson4/index.js
+++ lesson4/index.js
@@ -1,8 +1,11 @@
 const express=require('express')
 const app=express()
 const PersonService=require('../services/person-service')
+const bodyParser=require('body-parser')
 
 app.set('view engine','pug')
+app.use(bodyParser.json())
+
 app.get('/',(req,res)=>{
     // res.send('hello')
     // res.sendFile(__dirname+'/index.html')
@@ -10,7 +13,25 @@ app.get('/',(req,res)=>{
  })
 app.get('/person/all',async (req,res)=>{
     const people=await PersonService.findAll()
-    res.send(people)
+   // res.send(people)
+   res.render('person',{person:people})
+})
+app.get('/person/:id',async (req,res)=>{
+    const id=req.params.id
+    const person=await PersonService.find(id)
+    res.send(person)
+   //res.render('person',{person:people})
+})
+
+app.post('/person',async (req,res)=>{
+    console.log(req.body)
+    const p=await PersonService.add(req.body)
+    res.send(p)
+})
+
+app.delete('/person/:id',async (req,res)=>{
+    await PersonService.del(req.params.id)
+    res.send('ok')
 })
 
 app.listen(3000,()=>{
